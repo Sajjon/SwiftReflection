342a73514ab474e12daaac0df163797b04bf16c7
Sajjon <alex.cyon@gmail.com>
2016-09-02 17:16:12 +0200

    removed need for use of class calling of method

--- SwiftReflection/SwiftReflection.swift
+++ SwiftReflection/SwiftReflection.swift
@@ -9,10 +9,6 @@
 import Foundation
 import ObjectiveC.runtime
 
-protocol ReflectionProtocol {
-    static func getTypesOfProperties(inClass clazz: NSObject.Type) -> Dictionary<String, Any>?
-    static func getTypesOfProperties(ofObject object: NSObject) -> Dictionary<String, Any>?
-}
 
 /*
  Please note that the class you are inspecting have to inherit from NSObject.
@@ -22,47 +18,88 @@ protocol ReflectionProtocol {
 
  This tool can also find name and type of "primitive data types" such as Bool, Int, Int32, _HOWEVER_ it does not work if said primitive have optional type, e.g. Int? <--- DOES NOT WORK
  */
-open class SwiftReflection: ReflectionProtocol {
 
-    public class func getTypesOfProperties(inClass clazz: NSObject.Type) -> Dictionary<String, Any>? {
-        var count = UInt32()
-        guard let properties = class_copyPropertyList(clazz, &count) else { return nil }
-        var types: Dictionary<String, Any> = [:]
-        for i in 0..<Int(count) {
-            guard let property: objc_property_t = properties[i], let name = getNameOf(property: property) else { continue }
-            let type = getTypeOf(property: property)
-            types[name] = type
-        }
-        free(properties)
-        return types
-    }
 
-    public class func getTypesOfProperties(ofObject object: NSObject) -> Dictionary<String, Any>? {
-        let clazz: NSObject.Type = type(of: object)
-        return getTypesOfProperties(inClass: clazz)
+public func getTypesOfProperties(in clazz: NSObject.Type) -> Dictionary<String, Any>? {
+    var count = UInt32()
+    guard let properties = class_copyPropertyList(clazz, &count) else { return nil }
+    var types: Dictionary<String, Any> = [:]
+    for i in 0..<Int(count) {
+        guard let property: objc_property_t = properties[i], let name = getNameOf(property: property) else { continue }
+        let type = getTypeOf(property: property)
+        types[name] = type
     }
+    free(properties)
+    return types
 }
 
-private extension SwiftReflection {
-    class func getTypeOf(property: objc_property_t) -> Any {
-        guard let attributesAsNSString: NSString = NSString(utf8String: property_getAttributes(property)) else { return Any.self }
-        let attributes = attributesAsNSString as String
-        let slices = attributes.components(separatedBy: "\"")
-        guard slices.count > 1 else { return getPrimitiveDataType(withAttributes: attributes) }
-        let objectClassName = slices[1]
-        let objectClass = NSClassFromString(objectClassName) as! NSObject.Type
-        return objectClass
-    }
+public func getTypesOfProperties(ofObject object: NSObject) -> Dictionary<String, Any>? {
+    let clazz: NSObject.Type = type(of: object)
+    return getTypesOfProperties(in: clazz)
+}
 
-    class func getPrimitiveDataType(withAttributes attributes: String) -> Any {
-        guard let letter = attributes.substring(from: 1, to: 2), let type = primitiveDataTypes[letter] else { return Any.self }
-        return type
-    }
+public func typeOf(property propertyName: String, for object: NSObject) -> Any? {
+    let type = type(of: object)
+    return typeOf(property: propertyName, in: type)
+}
 
-    class func getNameOf(property: objc_property_t) -> String? {
-        guard let name: NSString = NSString(utf8String: property_getName(property)) else { return nil }
-        return name as String
-    }
+public func typeOf(property propertyName: String, in clazz: NSObject.Type) -> Any? {
+    guard let propertyTypes = getTypesOfProperties(in: clazz), let type = propertyTypes[propertyName] else { return nil }
+    print("Property named: '\(propertyName)' has type: \(type)")
+    return type
+}
+
+public func isProperty(named propertyName: String, ofType targetType: Any, for object: NSObject) -> Bool {
+    let type = type(of: object)
+    return isProperty(named: propertyName, ofType: targetType, in: type)
+}
+
+public func isProperty(named propertyName: String, ofType targetType: Any, in clazz: NSObject.Type) -> Bool {
+    let propertyType = typeOf(property: propertyName, in: clazz)
+    let match = propertyType == targetType
+    return match
+}
+
+fileprivate func ==(rhs: Any, lhs: Any) -> Bool {
+    var rhsType: String = "\(rhs)".withoutOptional
+    var lhsType: String = "\(lhs)".withoutOptional
+    let same = rhsType == lhsType
+    return same
+}
+
+fileprivate func ==(rhs: NSObject.Type, lhs: Any) -> Bool {
+    let rhsType: String = "\(rhs)".withoutOptional
+    let lhsType: String = "\(lhs)".withoutOptional
+    let same = rhsType == lhsType
+    return same
+}
+
+fileprivate func ==(rhs: Any, lhs: NSObject.Type) -> Bool {
+    let rhsType: String = "\(rhs)".withoutOptional
+    let lhsType: String = "\(lhs)".withoutOptional
+    let same = rhsType == lhsType
+    return same
+}
+
+
+fileprivate func getTypeOf(property: objc_property_t) -> Any {
+    guard let attributesAsNSString: NSString = NSString(utf8String: property_getAttributes(property)) else { return Any.self }
+    let attributes = attributesAsNSString as String
+    let slices = attributes.components(separatedBy: "\"")
+    guard slices.count > 1 else { return getPrimitiveDataType(withAttributes: attributes) }
+    let objectClassName = slices[1]
+    let objectClass = NSClassFromString(objectClassName) as! NSObject.Type
+    return objectClass
+}
+
+fileprivate func getPrimitiveDataType(withAttributes attributes: String) -> Any {
+    guard let letter = attributes.substring(from: 1, to: 2), let type = primitiveDataTypes[letter] else { return Any.self }
+    return type
+}
+
+fileprivate func getNameOf(property: objc_property_t) -> String? {
+    guard let name: NSString = NSString(utf8String: property_getName(property)) else { return nil }
+    return name as String
 }
 
 fileprivate let primitiveDataTypes: Dictionary<String, Any> = [
@@ -79,9 +116,18 @@ fileprivate let primitiveDataTypes: Dictionary<String, Any> = [
     "{" : Decimal.self
 ]
 
-extension String {
+private extension String {
     func substring(from fromIndex: Int, to toIndex: Int) -> String? {
         let substring = self[self.index(self.startIndex, offsetBy: fromIndex)..<self.index(self.startIndex, offsetBy: toIndex)]
         return substring
     }
+
+    /// Extracts "NSDate" from the string "Optional(NSDate)"
+    var withoutOptional: String {
+        guard self.contains("Optional(") && self.contains(")") else { return self }
+        let afterOpeningParenthesis = self.components(separatedBy: "(")[1]
+        let wihtoutOptional = afterOpeningParenthesis.components(separatedBy: ")")[0]
+        return wihtoutOptional
+    }
+    
 }
